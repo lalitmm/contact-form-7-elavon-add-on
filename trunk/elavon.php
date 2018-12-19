58985ff47eebbd1d4b7419afb392f6f6276d2780
lalitmm <lalit.murmu@aol.com>
2018-12-19 12:53:50 -0500

    first commit

---
+++ trunk/elavon.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+Plugin Name: Contact Form 7 - Elavon Add-on
+Plugin URI: 
+Description: Integrates Elavon with Contact Form 7
+Author: 
+Author URI: 
+License: GPL2
+Version: 1.6.1
+*/
+
+/*  Copyright 2018 Roventix Inc
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*/
+
+
+
+// plugin variable: cf7elavon
+
+// empty function used by pro version to check if free version is installed
+function cf7pp_free() {
+}
+
+// check if pro version is attempting to be activated - if so, then deactive this plugin
+if (function_exists('cf7elavon_pro')) {
+
+	deactivate_plugins('contact-form-7-elavon-add-on-pro/paypal.php');
+
+} else {
+
+	//  plugin functions
+	register_activation_hook( 	__FILE__, "cf7elavon_activate" );
+	register_deactivation_hook( __FILE__, "cf7elavon_deactivate" );
+	register_uninstall_hook( 	__FILE__, "cf7elavon_uninstall" );
+
+	function cf7elavon_activate() {
+		
+		// default options
+		$cf7elavon_options = array(
+			'currency'    		=> '25',
+			'language'    		=> '3',
+			'liveaccount'    	=> '',
+			'sandboxaccount'    => '',
+			'mode' 				=> '2',
+			'cancel'    		=> '',
+			'return'    		=> '',
+			'redirect'			=> '2',
+			'pub_key_live'		=> '',
+			'sec_key_live'		=> '',
+			'pub_key_test'		=> '',
+			'sec_key_test'		=> '',
+		);
+		
+		add_option("cf7elavon_options", $cf7elavon_options);
+		
+	}
+
+	function cf7elavon_deactivate() {
+		
+		delete_option("cf7elavon_my_plugin_notice_shown");
+		
+	}
+
+	function cf7elavon_uninstall() {
+	}
+
+	// check to make sure contact form 7 is installed and active
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {		
+		
+		// public includes
+		include_once('includes/functions.php');
+		include_once('includes/redirect_methods.php');
+		include_once('includes/redirect.php');
+		include_once('includes/enqueue.php');
+		
+		
+		// admin includes
+		if (is_admin()) {
+			include_once('includes/admin/tabs_page.php');
+			include_once('includes/admin/settings_page.php');
+			include_once('includes/admin/menu_links.php');
+			include_once('includes/admin/extensions.php');
+		}
+		
+		
+		// start session if not already started
+		function cf7elavon_session() {
+			if(!session_id()) {
+				session_start();
+			}
+		}
+		add_action('init', 'cf7elavon_session', 1);
+		
+		
+	} else {
+		
+		// give warning if contact form 7 is not active
+		function cf7elavon_my_admin_notice() {
+			?>
+			<div class="error">
+				<p><?php _e( '<b>Contact Form 7 - Elavon Add-on:</b> Contact Form 7 is not installed and / or active! Please install <a target="_blank" href="https://wordpress.org/plugins/contact-form-7/">Contact Form 7</a>.', 'cf7pp' ); ?></p>
+			</div>
+			<?php
+		}
+		add_action( 'admin_notices', 'cf7elavon_my_admin_notice' );
+		
+	}
+}
+
+
+?
---
+++ trunk/readme.txt
@@ -0,0 +1,80 @@
+=== Contact Form 7 - Elavon Add-on ===
+Contributors: 
+Donate link: 
+Tags: elavon, contact form 7, contact form, contact form paypal
+Author URI: 
+Requires at least: 3.0
+Tested up to: 5.0
+Requires PHP: 5.4
+Stable tag: 1.6.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Integrates Elavon with Contact Form 7. Start selling with Elavon today. 
+
+== Description ==
+= Overview =
+
+This PayPal plugin seamlessly integrates Elavon with Contact Form 7.
+
+Watch this 1 minute video of how the plugin works:
+
+[]
+
+If you have any problems, questions, or issues about this plugin then please create a support request and we will get back to you quickly.
+
+Various trademarks held by their respective owners.
+
+Note: This PayPal & Stripe plugin works with both the old and new Contact Form 7 interface. A PayPal account, which is free, is required to use PayPal. A Stripe account, which is also free, is required to use Stripe. You can use the plugin with only PayPal enabled, only Stripe or PayPal and Stripe at the same time.
+
+= Contact Form 7 - Elavon Integration Add-on Features =
+
+*	Set items name, price, SKU/ID per contact form
+*   Built in support for 18 languages (PayPal currently supports 18 languages)
+*   Built in support 25 currencies (PayPal currently supports 25 currencies)
+*	PayPal testing through SandBox
+*	Choose a cancel payment URL
+*	Choose a succesful payment URL
+
+> #### Contact Form 7 - PayPal & Stripe Integration Add-on Pro
+> We offer a Pro version of this PayPal plugin for business owners who need more features.<br />
+>
+> * Only send Contact Form 7 email if PayPal payment is successful.
+> * Link a form item to quantity - A form item can be a textbox, dropdown, radio button, etc. anything that is a number.<br />
+> * Link a form item to price - The price field can be linked to any form item. Pipes are supported to allow for dropdown text options for each price.<br />
+> * Link form items to option text fields - The text field can be linked to any form item.<br />
+> * Charge Fixed Tax <br />
+> * Charge Tax Rate <br />
+> * Charge Shipping & Handling per item<br />
+> * Hide Customer's Custom Note Field<br />
+> *	Choose a cancel payment URL per contact form<br />
+> *	Choose a succesful payment URL per contact form<br />
+>
+>
+> [You can learn more about Contact Form 7 - PayPal & Stripe Integration Add-on Pro here](https://wpplugin.org/downloads/contact-form-7-paypal-add-on/)
+
+
+
+
+== Installation ==
+
+= Automatic Installation =
+> 1. Sign in to your WordPress site as an administrator.
+> 2. In the main menu go to Plugins -> Add New.
+> 3. Search for Contact Form 7 - PayPal Add-on and click install.
+> 4. That's it. You are now ready to start accepting PayPal payment on your website through your contact form.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Screenshots ==
+1. Options while editing a contact form - Note: This plugin works with both the old and new Contact Form 7 interface, screenshots from old version.
+2. PayPal settings page
+3. Stripe settings page
+
+
+== Changelog ==
+
+= 1.0 =
+Initial releas
